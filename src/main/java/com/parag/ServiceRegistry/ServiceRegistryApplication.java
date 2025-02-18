37c31607543944ebe2c0fb0b052e6b531b94c6ea
ParagShah97 <parag.shah97@gmail.com>
2025-02-17 20:01:41 -0500

    added comment

--- src/main/java/com/parag/ServiceRegistry/ServiceRegistryApplication.java
+++ src/main/java/com/parag/ServiceRegistry/ServiceRegistryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer	// Service Registry will work as Eureka Server.
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
